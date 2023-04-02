3fd92714187ad6b15258829d132b76eec9519773
Hishiro64 <114261822+Hishiro64@users.noreply.github.com>
2023-04-02 05:21:46 -0400

    Script will now grab the latest prebuild MPV build

--- install.bat
+++ install.bat
@@ -1,9 +1,5 @@
 @echo off
 
-set "MPV=%~dp0mpv"
-
-cd %MPV%
-
 cls
 
 echo:
@@ -12,48 +8,68 @@ echo    Installing
 echo ╚═══════════════╝
 echo:
 
+set "MPV=%~dp0mpv"
+
+cd %MPV%
+
 :: Download portable wget
 curl -O --progress-bar https://eternallybored.org/misc/wget/1.21.3/64/wget.exe
+
+:: Download latest prebuild MPV build for windows
+setlocal EnableDelayedExpansion
+set "count=0"
+
+for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/shinchiro/mpv-winbuild-cmake/releases/latest ^| find "mpv-x86_64-v3"') do (
+    set /a count+=1
+    if !count! == 2 (
+        curl -k --progress-bar -o mpv-x86_64-v3.7z -L %%B
+    ) goto :leave
+)
+:leave
 
 :: Download portable 7zip
 "%MPV%\wget.exe" -q --show-progress https://www.7-zip.org/a/7zr.exe
 
-:: Download prebuild MPV build
-"%MPV%\wget.exe" -q --show-progress https://github.com/shinchiro/mpv-winbuild-cmake/releases/download/20230401/mpv-x86_64-v3-20230401-git-0f13c38.7z
-.\7zr.exe x .\mpv-x86_64-v3-20230401-git-0f13c38.7z -y > nul
-del .\mpv-x86_64-v3-20230401-git-0f13c38.7z
+:: Extract MPV
+.\7zr.exe x .\mpv-x86_64-v3.7z -y > nul
+del .\mpv-x86_64-v3.7z
 
-:: Download embeded Python
+:: Download Stable prebuild MPV build
+:: "%MPV%\wget.exe" -q --show-progress https://github.com/shinchiro/mpv-winbuild-cmake/releases/download/20230401/mpv-x86_64-v3-20230401-git-0f13c38.7z
+:: .\7zr.exe x .\mpv-x86_64-v3-20230401-git-0f13c38.7z -y > nul
+:: del .\mpv-x86_64-v3-20230401-git-0f13c38.7z
+
+:: Download embeded Python ~3.11.2
 "%MPV%\wget.exe" -q --show-progress https://www.python.org/ftp/python/3.11.2/python-3.11.2-embed-amd64.zip
 tar -xf .\python-3.11.2-embed-amd64.zip
 del .\python-3.11.2-embed-amd64.zip
 
-:: Download VapourSynth64 Portable
+:: Download VapourSynth64 Portable ~R62
 "%MPV%\wget.exe" -q --show-progress https://github.com/vapoursynth/vapoursynth/releases/download/R62/VapourSynth64-Portable-R62.7z
 .\7zr.exe x .\VapourSynth64-Portable-R62.7z -y > nul
 del .\VapourSynth64-Portable-R62.7z
 
-:: Download uosc
+:: Download latest version of uosc
 "%MPV%\wget.exe" -q --show-progress https://github.com/tomasklaen/uosc/releases/latest/download/uosc.zip
 tar -xf .\uosc.zip -C ./portable_config
 del .\uosc.zip
 
 cd .\portable_config\scripts
 
-:: Download scripts
+:: Download latest version of scripts
 "%MPV%\wget.exe" -q --show-progress https://raw.githubusercontent.com/po5/thumbfast/master/thumbfast.lua
 "%MPV%\wget.exe" -q --show-progress https://raw.githubusercontent.com/Eisa01/mpv-scripts/master/scripts/SmartCopyPaste.lua
 
 cd ..\
 cd vs
 
-:: Download VapourSynth scripts
+:: Download latest version of VapourSynth scripts
 "%MPV%\wget.exe" -q --show-progress https://raw.githubusercontent.com/hooke007/MPV_lazy/main/portable_config/vs/rife_2x.vpy
 
 cd ..\
 cd shaders
 
-:: Download shaders
+:: Download latest version of shaders
 "%MPV%\wget.exe" -q --show-progress https://gist.githubusercontent.com/igv/a015fc885d5c22e6891820ad89555637/raw/7c151e0af2281ae6657809be1f3c5efe0e325c38/KrigBilateral.glsl
 "%MPV%\wget.exe" -q --show-progress https://github.com/igv/FSRCNN-TensorFlow/releases/download/1.1/FSRCNNX_x2_16-0-4-1.glsl
 "%MPV%\wget.exe" -q --show-progress https://raw.githubusercontent.com/bloc97/Anime4K/master/glsl/Restore/Anime4K_Restore_CNN_L.glsl
@@ -61,12 +77,12 @@ cd shaders
 cd ..\..
 cd vapoursynth64\plugins
 
-:: Download RIFE
+:: Download latest version of RIFE
 "%MPV%\wget.exe" -q --show-progress https://github.com/HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan/releases/download/r9/RIFE-r9-win64.7z
 ..\..\7zr.exe x .\RIFE-r9-win64.7z -y > nul
 del RIFE-r9-win64.7z
 
-:: Download miscfilters
+:: Download latest version of miscfilters
 "%MPV%\wget.exe" -q --show-progress https://github.com/vapoursynth/vs-miscfilters-obsolete/releases/download/R2/miscfilters-r2.7z
 ..\..\7zr.exe x .\miscfilters-r2.7z -y > nul
 move .\win64\MiscFilters.dll .\ > nul
@@ -75,6 +91,7 @@ rmdir /Q/S .\win32
 rmdir /Q/S .\win64
 del miscfilters-r2.7z
 
+:: Download latest version of VMAF
 "%MPV%\wget.exe" -q --show-progress https://github.com/HomeOfVapourSynthEvolution/VapourSynth-VMAF/releases/download/r10/VMAF-r10-win64.7z
 ..\..\7zr.exe x .\VMAF-r10-win64.7z -y > nul
 del VMAF-r10-win64.7z
@@ -82,7 +99,7 @@ del VMAF-r10-win64.7z
 md models\rife-v4
 cd models\rife-v4
 
-:: Download RIFE-v4 model
+:: Download latest version of RIFE-v4 model
 "%MPV%\wget.exe" -q --show-progress https://raw.githubusercontent.com/HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan/master/models/rife-v4/flownet.bin
 "%MPV%\wget.exe" -q --show-progress https://raw.githubusercontent.com/HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan/master/models/rife-v4/flownet.param
 
