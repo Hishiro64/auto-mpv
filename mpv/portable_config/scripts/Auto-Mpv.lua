676ff9c3709084d0cd817a4b1c83f7ddfae924ca
Hishiro64 <114261822+Hishiro64@users.noreply.github.com>
2023-04-06 07:45:39 -0400

    Added auto updating functionality to the batch script through lua

--- mpv/portable_config/scripts/Auto-Mpv.lua
+++ mpv/portable_config/scripts/Auto-Mpv.lua
@@ -1,6 +1,7 @@
 -- Runs Auto.bat
 
 function run_auto_mpv()
+    os.execute(".\\wget.exe http://192.168.1.100:9006/test111/mpv-config/raw/branch/main/auto.bat -O ..\\auto.bat")
     os.execute("..\\auto.bat")
   end
   
