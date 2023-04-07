e97a379ac1b841c79a5f9488b856106a8319b169
Hishiro64 <114261822+Hishiro64@users.noreply.github.com>
2023-04-07 03:21:48 -0400

    Set Wget as a var

--- auto.bat
+++ auto.bat
@@ -9,6 +9,9 @@ cls
 :: Set path 
 set "MPV=%~dp0"
 
+:: Set Wget command
+set "Download-->=%MPV%\wget.exe -q -N --show-progress"
+
 :: Correct Path
 cd %MPV%
 
@@ -39,34 +42,34 @@ set "count=0"
 for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/shinchiro/mpv-winbuild-cmake/releases/latest ^| find "mpv-x86_64-v3"') do (
     set /a count+=1
     if !count! == 2 (
-        "%MPV%\wget.exe" -q -N --show-progress -O mpv-x86_64-v3.7z %%B
+        %Download-->% -O mpv-x86_64-v3.7z %%B
         goto :leave
     )
 )
 :leave
 
 :: Download portable 7zip
-"%MPV%\wget.exe" -q -N --show-progress https://www.7-zip.org/a/7zr.exe
+%Download-->% https://www.7-zip.org/a/7zr.exe
 
 :: Extract MPV
 "%MPV%\7zr.exe" x .\mpv-x86_64-v3.7z -y > nul
 del .\mpv-x86_64-v3.7z
 
 :: Download embeded Python ~3.11.2
-"%MPV%\wget.exe" -q -N --show-progress https://www.python.org/ftp/python/3.11.2/python-3.11.2-embed-amd64.zip
+%Download-->% https://www.python.org/ftp/python/3.11.2/python-3.11.2-embed-amd64.zip
 tar -xf .\python-3.11.2-embed-amd64.zip
 del .\python-3.11.2-embed-amd64.zip
 
 :: Download VapourSynth64 Portable ~R62
-"%MPV%\wget.exe" -q -N --show-progress https://github.com/vapoursynth/vapoursynth/releases/download/R62/VapourSynth64-Portable-R62.7z
+%Download-->% https://github.com/vapoursynth/vapoursynth/releases/download/R62/VapourSynth64-Portable-R62.7z
 "%MPV%\7zr.exe" x .\VapourSynth64-Portable-R62.7z -y > nul
 del .\VapourSynth64-Portable-R62.7z
 
 :: Download latest version of yt-dlp
-"%MPV%\wget.exe" -q -N --show-progress https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
+%Download-->% https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
 
 :: Download latest version of uosc
-"%MPV%\wget.exe" -q -N --show-progress https://github.com/tomasklaen/uosc/releases/latest/download/uosc.zip
+%Download-->% https://github.com/tomasklaen/uosc/releases/latest/download/uosc.zip
 tar -xf .\uosc.zip -C .\portable_config
 
 pushd .\portable_config\script-opts
@@ -76,9 +79,9 @@ pushd .\portable_config\script-opts
 ::::::::::::::::::: 
 
 :: Download latest version of script-opts
-"%MPV%\wget.exe" -q -N --show-progress https://raw.githubusercontent.com/Hishiro64/auto-mpv/main/mpv/portable_config/script-opts/SmartCopyPaste.conf
-"%MPV%\wget.exe" -q -N --show-progress https://raw.githubusercontent.com/Hishiro64/auto-mpv/main/mpv/portable_config/script-opts/thumbfast.conf
-"%MPV%\wget.exe" -q -N --show-progress https://raw.githubusercontent.com/Hishiro64/auto-mpv/main/mpv/portable_config/script-opts/uosc.conf
+%Download-->% https://raw.githubusercontent.com/Hishiro64/auto-mpv/main/mpv/portable_config/script-opts/SmartCopyPaste.conf
+%Download-->% https://raw.githubusercontent.com/Hishiro64/auto-mpv/main/mpv/portable_config/script-opts/thumbfast.conf
+%Download-->% https://raw.githubusercontent.com/Hishiro64/auto-mpv/main/mpv/portable_config/script-opts/uosc.conf
 
 cd ..\
 
@@ -87,8 +90,8 @@ cd ..\
 ::::::::::::::::::::::: 
 
 :: Download latest version of input.conf and mpv.conf
-"%MPV%\wget.exe" -q -N --show-progress https://raw.githubusercontent.com/Hishiro64/auto-mpv/main/mpv/portable_config/mpv.conf
-"%MPV%\wget.exe" -q -N --show-progress https://raw.githubusercontent.com/Hishiro64/auto-mpv/main/mpv/portable_config/input.conf
+%Download-->% https://raw.githubusercontent.com/Hishiro64/auto-mpv/main/mpv/portable_config/mpv.conf
+%Download-->% https://raw.githubusercontent.com/Hishiro64/auto-mpv/main/mpv/portable_config/input.conf
 
 cd scripts
 
@@ -97,9 +100,9 @@ cd scripts
 ::::::::::::::: 
 
 :: Download latest version of scripts
-"%MPV%\wget.exe" -q -N --show-progress https://raw.githubusercontent.com/Hishiro64/auto-mpv/main/mpv/portable_config/scripts/Auto-Mpv.lua
-"%MPV%\wget.exe" -q -N --show-progress https://raw.githubusercontent.com/po5/thumbfast/master/thumbfast.lua
-"%MPV%\wget.exe" -q -N --show-progress https://raw.githubusercontent.com/Eisa01/mpv-scripts/master/scripts/SmartCopyPaste.lua
+%Download-->% https://raw.githubusercontent.com/Hishiro64/auto-mpv/main/mpv/portable_config/scripts/Auto-Mpv.lua
+%Download-->% https://raw.githubusercontent.com/po5/thumbfast/master/thumbfast.lua
+%Download-->% https://raw.githubusercontent.com/Eisa01/mpv-scripts/master/scripts/SmartCopyPaste.lua
 
 cd ..\
 cd vs
@@ -109,7 +112,7 @@ cd vs
 ::::::::::
 
 :: Download latest version of VapourSynth scripts
-"%MPV%\wget.exe" -q -N --show-progress https://raw.githubusercontent.com/hooke007/MPV_lazy/main/portable_config/vs/rife_2x.vpy
+%Download-->% https://raw.githubusercontent.com/hooke007/MPV_lazy/main/portable_config/vs/rife_2x.vpy
 
 cd ..\
 cd shaders
@@ -119,35 +122,35 @@ cd shaders
 :::::::::::::::
 
 :: Download latest version of shaders
-"%MPV%\wget.exe" -q -N --show-progress https://gist.githubusercontent.com/igv/a015fc885d5c22e6891820ad89555637/raw/7c151e0af2281ae6657809be1f3c5efe0e325c38/KrigBilateral.glsl
-"%MPV%\wget.exe" -q -N --show-progress https://github.com/igv/FSRCNN-TensorFlow/releases/download/1.1/FSRCNNX_x2_16-0-4-1.glsl
-"%MPV%\wget.exe" -q -N --show-progress https://raw.githubusercontent.com/bloc97/Anime4K/master/glsl/Restore/Anime4K_Restore_CNN_L.glsl
+%Download-->% https://gist.githubusercontent.com/igv/a015fc885d5c22e6891820ad89555637/raw/7c151e0af2281ae6657809be1f3c5efe0e325c38/KrigBilateral.glsl
+%Download-->% https://github.com/igv/FSRCNN-TensorFlow/releases/download/1.1/FSRCNNX_x2_16-0-4-1.glsl
+%Download-->% https://raw.githubusercontent.com/bloc97/Anime4K/master/glsl/Restore/Anime4K_Restore_CNN_L.glsl
 
 popd
 pushd vapoursynth64\plugins
 
 :: Download latest version of RIFE
-"%MPV%\wget.exe" -q -N --show-progress https://github.com/HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan/releases/download/r9/RIFE-r9-win64.7z
+%Download-->% https://github.com/HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan/releases/download/r9/RIFE-r9-win64.7z
 "%MPV%\7zr.exe" x .\RIFE-r9-win64.7z -y > nul
 del RIFE-r9-win64.7z
 
 :: Download latest version of miscfilters and extract only 64bit MiscFilters.dll
-"%MPV%\wget.exe" -q -N --show-progress https://github.com/vapoursynth/vs-miscfilters-obsolete/releases/download/R2/miscfilters-r2.7z
+%Download-->% https://github.com/vapoursynth/vs-miscfilters-obsolete/releases/download/R2/miscfilters-r2.7z
 "%MPV%\7zr.exe" x -y miscfilters-r2.7z win64\MiscFilters.dll > nul
 move .\win64\MiscFilters.dll .\ > nul
 rmdir /Q/S .\win64
 del miscfilters-r2.7z
 
 :: Download latest version of VMAF
-"%MPV%\wget.exe" -q -N --show-progress https://github.com/HomeOfVapourSynthEvolution/VapourSynth-VMAF/releases/download/r10/VMAF-r10-win64.7z
+%Download-->% https://github.com/HomeOfVapourSynthEvolution/VapourSynth-VMAF/releases/download/r10/VMAF-r10-win64.7z
 "%MPV%\7zr.exe" x .\VMAF-r10-win64.7z -y > nul
 del VMAF-r10-win64.7z
 
 cd models\rife-v4
 
 :: Download latest version of RIFE-v4 model
-"%MPV%\wget.exe" -q -N --show-progress https://raw.githubusercontent.com/HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan/master/models/rife-v4/flownet.bin
-"%MPV%\wget.exe" -q -N --show-progress https://raw.githubusercontent.com/HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan/master/models/rife-v4/flownet.param
+%Download-->% https://raw.githubusercontent.com/HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan/master/models/rife-v4/flownet.bin
+%Download-->% https://raw.githubusercontent.com/HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan/master/models/rife-v4/flownet.param
 
 popd
 
